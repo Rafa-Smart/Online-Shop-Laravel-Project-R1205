3edded82436dbe8ba24787a7a7d4700492506f5c
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-12-18 12:41:02 +0700

    test

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -868,7 +868,7 @@ input[type="range"]:active::-webkit-slider-thumb {
                             <div class="discount-badge">-{{ $discount }}%</div>
                         @endif
                         <div class="slide-left">
-                            <img src="{{ $ad->bg_image ? (filter_var($ad->bg_image, FILTER_VALIDATE_URL) ? $ad->bg_image : asset('storage/' . $ad->bg_image)) : asset('img/sate-ayam.png') }}" alt="Ad Image">
+                            <img src="{{ asset('storage/' . $ad->bg_image) ? asset('img/sate-ayam.png') }}" alt="Ad Image">
 
                         </div>
                         <div class="slide-right">
